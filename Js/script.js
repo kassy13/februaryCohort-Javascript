dbd052678ce2e0a6fa37f89a37aa266102c2a1c5
kassycodes13 <ichokusomtoo12@gmail.com>
2024-05-10 12:35:29 +0100

    first commit

---
+++ Js/script.js
@@ -0,0 +1,72 @@
+// Datatypes in javascript
+// 2 main Datatypes in js which are the primitive and reference/Object type
+
+// Primitive datatypes
+// Numbers
+// String
+// Boolean
+// Undefined
+// Null
+// Big int
+// Symbol
+
+let num = 23;
+console.log(num);
+let student = "obi";
+let greeting = "what's";
+let g = false;
+let b;
+console.log(b);
+
+// LET
+// let age = 20;
+// console.log(age);
+// age = "boy";
+// console.log(age);
+
+// CONST
+const birthYear = 1900;
+console.log(birthYear);
+
+// VAR
+var job = "Programmer";
+console.log(job);
+job = "teacher";
+console.log(job);
+
+// Block scope
+{
+  let age = 20;
+  console.log(typeof age);
+}
+{
+  let age = "age";
+  console.log(age);
+}
+
+//
+const numb = "23";
+console.log(typeof numb);
+
+const last_name = "f";
+const new3 = 33;
+let MYname = "somtoo";
+
+// Arithmetic operators
+console.log(2 + 4);
+console.log(4 - 39);
+const year = 2024;
+const ageNigeria = year - 1960;
+console.log("Nigeria age", ageNigeria);
+console.log(year / 10);
+console.log(year * 2);
+
+// Modulus
+console.log(7 % 2);
+console.log(10 % 4);
+let x = 10;
+console.log(x++);
+console.log(x);
+console.log(--x);
+
+console.log(x);
