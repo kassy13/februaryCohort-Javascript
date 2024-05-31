d422f28b0186206765ce4158ddb7db6aa3402742
kassycodes13 <ichokusomtoo12@gmail.com>
2024-05-31 12:10:08 +0100

    Solution to assignment

--- Js/datastructures.js
+++ Js/datastructures.js
@@ -32,3 +32,13 @@ console.log(friends);
 const meMyselfAndI = ["somtoo", "obi", 20024 - 1970, "instructor", friends];
 console.log(meMyselfAndI);
 console.log(meMyselfAndI[4][2]);
+
+// SOLUTION
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(tips)
+const totals = [bills[0] + tips[0],bills[1] + tips[1], bills[2] + tips[2]];
+console.log(totals)
